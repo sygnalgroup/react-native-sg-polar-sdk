be46b5d5e9265648a4e2618a3c520f9c3170fd95
Leonardo Maron <leo22.maron@hotmail.com>
2024-04-11 16:56:37 -0300

    feat: add exports to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
-import { NativeModules, Platform } from 'react-native';
+import { NativeModules, Platform, NativeEventEmitter } from 'react-native';
+import type { EmitterSubscription } from 'react-native';
 
 const LINKING_ERROR =
   `The package 'react-native-sg-polar-sdk' doesn't seem to be linked. Make sure: \n\n` +
@@ -17,6 +18,38 @@ const SgPolarSdk = NativeModules.SgPolarSdk
       }
     );
 
-export function multiply(a: number, b: number): Promise<number> {
-  return SgPolarSdk.multiply(a, b);
-}
+const polarEmmiter = new NativeEventEmitter(SgPolarSdk);
+
+type PolarEvent =
+  | 'connectionState'
+  | 'listExerciseItem'
+  | 'listExerciseComplete'
+  | 'searchDeviceItem'
+  | 'searchDeviceStatus'
+  | 'bleState'
+  | 'hrStatus';
+
+export const connectToDevice: (id: string) => void = SgPolarSdk.connectToDevice;
+
+export const disconnectFromDevice: (id: string) => void =
+  SgPolarSdk.disconnectFromDevice;
+
+export const startDevicesSearch: () => void = SgPolarSdk.startDevicesSearch;
+
+export const stopDevicesSearch: () => void = SgPolarSdk.stopDevicesSearch;
+
+export const broadcastToggle: () => void = SgPolarSdk.broadcastToggle;
+
+export const getExercises: () => void = SgPolarSdk.listH10Exercises;
+
+export const deleteExercise: (entryId: string) => void =
+  SgPolarSdk.deleteExercise;
+
+export const checkBle: (callback: (ble: boolean) => {}) => void =
+  SgPolarSdk.checkBle;
+
+export const addListener: (
+  eventType: PolarEvent,
+  listener: (event: any) => void,
+  context?: Object
+) => EmitterSubscription = polarEmmiter.addListener;
